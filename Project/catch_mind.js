b82b12f3974499c0a6138c930dbdbfe25753dd3e
KYEONGHO SHIN <sinborninkr@gmail.com>
2023-05-12 17:49:45 +0900

    decided to do not use mdl  do visual design myself

--- Project/catch_mind.js
+++ Project/catch_mind.js
@@ -1,12 +1,45 @@
+/*
+할일
+1. is_login  ->쿠키로 변경 해야됨 
+2. 쿠키에 name, pw,username 집어넣기
+3. log_in/out 함수 완성
+*/
+let is_login=false;
 
-let text = document.getElementsByTagName("p");
-
-function fn_change(){
-  if(text[0].style.color=="grey"){
-    text[0].style.color="white";
-  } else {
-    text[0].style.color="grey";
+$(document).ready(function(){
+  // ADD / REMOVE COMPONENTS depends on the login status
+  console.log(document.getElementById('register'));
+  if(!is_login){
+    $(".mdl-layout__drawer").children('.mdl-layout-title').html('Guest')
+  }else{
+    $(".mdl-layout__drawer").children('.mdl-layout-title').html(userName);
   }
-}
 
-setInterval(fn_change, 1000);
+  $("#log-in").click(function(){
+    if(is_login){
+      log_out();
+    }else{
+      log_in();
+    }
+  });
+
+  function log_in(){
+    $("#register").detach();
+    $("#log-in").html('Log Out');
+    $("#log-in").attr('href','catch_mind_login.html');
+    $("#log-in")
+    log_in=true;
+
+  };
+
+  function log_out(){
+    $("#register").attach();
+    $("#log-in").html('Log In');
+    $("#log-in").attr('href','catch_mind_first.html');
+
+  };
+
+  function register(){
+
+  };
+});
