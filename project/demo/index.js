303aa47cf6e2fbd202a2bd3496e49335dc3ed38d
KYEONGHO SHIN <sinborninkr@gmail.com>
2023-06-01 17:46:57 +0900

    20230601 started project coding, working on moving divisions and remove
    things to do
    1. copy and remove the item in container when the item offset is at the edge of right and left side . (and reset the position of the container)
    2. put the copied one on the right or left ( ex) if the right border of the item is at the left 0, copy and remove and put the copyied one onto the last of container)
    
    *might be some additional works such as the division container should not move or other else...

---
+++ project/demo/index.js
@@ -0,0 +1,42 @@
+
+const panelProperty = {
+    width: "80%",
+};
+
+$(document).ready(function () {
+    let movingdiv = $("#scroller");
+    $("body").on("mouseover", function (e) {
+        let mouse_x_coordinate = e.pageX;
+        let mouse_y_coordinate = e.pageY;
+        $("#mouseprinter").text(
+            "mouse position:" + mouse_x_coordinate + "," + mouse_y_coordinate
+        );
+    });
+    $("body").on("mousewheel", function (e) {
+        if (e.originalEvent.deltaY > 0 || e.originalEvent.deltaX > 0) {
+            moveDiv(movingdiv, -20);
+        } else {
+            moveDiv(movingdiv, 20);
+        }
+        // hideDiv(movingdiv.children().first());
+        console.log(movingdiv.children().first().offset().left);
+    });
+});
+
+function moveDiv(targetDiv, num) {
+    if (!targetDiv) {
+        console.log("targetDiv is undefined or null");
+        return;
+    }
+
+    let currentX = $(targetDiv).position().left;
+    let currentX_new = currentX + num + "px";
+    targetDiv.css("left", currentX_new);
+    let firstborn = $(targetDiv).children().first();
+    if($(targetDiv).children().first().offset)
+}
+
+function hideDiv(targetDiv) {
+    const scroller = $("#scroller");
+    const targetRight = $(targetDiv).position().left + $(targetDiv).outerWidth();
+}
