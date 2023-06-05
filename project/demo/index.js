d6d684debf5c00f452537e47878ba67a36515e50
KYEONGHO SHIN <sinborninkr@gmail.com>
2023-06-05 15:10:48 +0900

    Revert "20230605 edit the mechanism"
    This reverts commit 3d29f337995dd97f43e8299dfc675ad74e94d961.

--- project/demo/index.js
+++ project/demo/index.js
@@ -1,126 +1,54 @@
+const panelProperty = {
+    width: "80%",
+};
 
-  
-  $(document).ready(function () {
-    const outerdiv = 0;
-    const movingdiv = $("#scroller");
-    
-    let panelProperty = {
-        width: "80%",
-        element_no : movingdiv.children().length
-      };
-    
+$(document).ready(function () {
+    let movingdiv = $("#scroller");
     $("body").on("mouseover", function (e) {
-      let mouse_x_coordinate = e.pageX;
-      let mouse_y_coordinate = e.pageY;
-      $("#mouseprinter").text(
-        "mouse position:" + mouse_x_coordinate + "," + mouse_y_coordinate
-      );
+        let mouse_x_coordinate = e.pageX;
+        let mouse_y_coordinate = e.pageY;
+        $("#mouseprinter").text(
+            "mouse position:" + mouse_x_coordinate + "," + mouse_y_coordinate
+        );
     });
-  
     $("body").on("mousewheel", function (e) {
-      let amount = 0;
-  
-      if (e.originalEvent.deltaY > 0 || e.originalEvent.deltaX > 0) {
-        amount = +20;
-      } else {
-        amount = -20;
-      }
-  
-      moveDiv(movingdiv, amount);
-      moveOuterDiv(movingdiv);
+        let amount = 0;
+        if (e.originalEvent.deltaY > 0 || e.originalEvent.deltaX > 0) {
+            amount = -20;
+        } else {
+            amount = 20
+        }
+        
+        moveDiv(movingdiv, amount);
+        if(movingdiv.first().offset().left+element_lf.outerWidth<0){
+            removeLeftmostElement(movingdiv);
+            movingdiv.css("left",0);
+        }
+        
     });
-  });
-  
-  // Move target division
-  function moveDiv(targetDiv, num) {
+});
+
+function moveDiv(targetDiv, num) {
     if (!targetDiv) {
-      console.log("targetDiv is undefined or null");
-      return;
+        console.log("targetDiv is undefined or null");
+        return;
     }
-  
-    targetDiv.children().each(function () {
-      const $child = $(this);
-      const $next_child = $child.after();
-      let child_prop = {
-            'width' : $child.outerWidth,
-            'left' : parseInt($child.css('left',10))
-      }
 
-      let next_child_prop = {
-            'width' : $next_child.outerWidth,
-            'left' : parseInt($next_child.css('left'),10)
-      }
+    let currentX = $(targetDiv).position().left;
+    let currentX_new = currentX + num + "px";
 
-      parseInt(next_child_prop['width'],10);
-      $child.after().css("left",child_prop['width']+next_child_prop['left']);
-      
-      const newLeft = child_prop['left'] + num;
-      const newNext = next_child_prop['left'] + child_prop['width'];
+    $(targetDiv).css("left", currentX_new);
+}
 
-      $child.css('left', newLeft + 'px');
-      $next_child.css('left',newNext+'px');
-    });
-  }
-  
-  // Move elements outside the target division
-  function moveOuterDiv(targetDiv) {
-    //get required information from the targetdivision
-    let contents = targetDiv.children();
-    let content;
-    let popout_dir;
+function removeLeftmostElement(targetDiv) {
+    let container = targetDiv;
+    let element_lf = container.children('.item').first();
+    let elf_right = element_lf.offset().left + element_lf.outerWidth();
     
-    //loop every content and find if there is something goes out of the
-    //tragetDiv
+    console.log("leftmost right : "+elf_right);
+    let containerLeft = container.position().left;
+    console.log("container's left : "+containerLeft);
+    leftmostElement.appendTo(targetDiv);
+    leftmostElement.remove(); // Remove the leftmost element
 
-    //container properties saved in a map 
-    //with offset : x-coordinate of left edge
-    //and width : x-coordinate of right edge - offset.  
-    let container_props = {
-        'offset' : targetDiv.offset().left,
-        'width' : targetDiv.innerWidth()
-    }
-    console.log('prop of container', container_props);
-    contents.each(function(i,e){
-    //element properties saved in anotherr map with the same key name.
-        const elem = $(e);
-        const elem_props = {
-            'offset' : elem.offset().left,
-            'width' : elem.outerWidth(),
-        };
-       // console.log("rightb of elem", elem_props['width']+ elem_props['offset']);
-
-        if(elem_props['offset']+elem_props['width']<container_props['offset']){
-            popout_dir = 'L';
-            content = elem;
-            return;
-        }else if(elem_props['offset']>=
-        container_props['offset']+container_props['width']){
-            popout_dir = 'R'
-            content = elem;
-            return;
-        }
-    });
-
-    //now if the popout_dir is L , move the division to the right side 
-    // R, move the division to the left side
-    console.log("popout_dir",popout_dir);
-    if(popout_dir){
-        if(popout_dir==='L'){
-            allign_contents(targetDiv,'L');
-            targetDiv.append(content);
-        }else if(popout_dir ==='R'){
-            allign_contents(targetDiv,'R');
-            targetDiv.prepend(content);
-        }
-    };
-
-  }
-
-  function allign_contents(targetDiv, direction){
-    if(direction === 'L'){
-        targetDiv.css("left",targetDiv.outerWidth+'px');
-    }else if(direction === 'R'){
-
-    }
-  }
-  
+}
